2fdb8a62bf2f0266a75ec27e972d68d2d2c6c437
Vlad Sh <kausvirtual@gmail.com>
2023-10-26 00:36:48 +0300

    MainHeader

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,3 +1,17 @@
 export default function MainHeader() {
-  return <header className="sticky top-0 bg-gray-100">main header</header>;
+  return (
+    <header className="sticky top-0 bg-gray-100">
+      <nav className="m-auto flex h-20 max-w-7xl items-center justify-between px-4 text-base font-semibold">
+        <div className="flex gap-8 capitalize">
+          <a href="#">dream pizza</a>
+          <a href="#">promo</a>
+          <a href="#">pizz</a>
+          <a href="#">drinks</a>
+          <a href="#">sides</a>
+          <a href="#">desert</a>
+        </div>
+        <div></div>
+      </nav>
+    </header>
+  );
 }
--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,24 +1,26 @@
 export default function TopHeader() {
   return (
     <header className="bg-neutral-800">
-      <nav className="m-auto flex h-20 max-w-7xl items-center justify-between px-4 text-base font-bold text-white">
+      <nav
+        className="m-auto flex h-20 max-w-7xl items-center justify-between px-4 text-base font-extrabold text-white"
+        role="navigation"
+      >
         <div className="flex gap-8">
-          <a href="tel:+12345678900">
-            <p>call 12345678900</p>
+          <a className="font-semibold" href="tel:+12345678900">
+            call 12345678900
           </a>
-          <a href="#">
-            <p>City</p>
+          <a href="#">City</a>
+          <a className="uppercase" href="#">
+            Pizza Tracker
           </a>
-          <a href="#">
-            <p className="uppercase">Pizza Tracker</p>
-          </a>
-          <a href="#">
-            <p className="uppercase">News</p>
+          <a className="uppercase" href="#">
+            News
           </a>
         </div>
         <div className="flex items-center gap-8">
           <p>language</p>
           <a
+            role="button"
             className="rounded-full bg-neutral-600  px-8 py-3 hover:bg-neutral-500"
             href="#"
           >
