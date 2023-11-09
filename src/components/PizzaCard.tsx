dad58ec0ff0237c46a891d6cc0111c34b32efaa2
Vlad Sh <kausvirtual@gmail.com>
2023-11-09 22:54:08 +0200

    store

--- src/components/PizzaCard.tsx
+++ src/components/PizzaCard.tsx
@@ -1,6 +1,18 @@
 type PizzaCardProps = {
-  
+  pizzaName: string;
+  size: "SMALL" | "STANDARD" | "LARGE" | "HUGE";
+  weightStandard: number;
+  priceStandard: number;
+  doughCrust: "THIN" | "THICK_CRUST" | "PHILADELPHIA" | "HOT_DOG_CRUST";
+  ingredients: {
+    id: number;
+    ingredientName: string;
+  };
+  pizzaAttributes: {
+    id: number;
+    attributeName: "CHEESE" | "VEGETARIAN" | "SPICY" | "NEW" | "NONE";
+  };
 };
-export default function PizzaCard({  }: PizzaCardProps) {
+export default function PizzaCard({}: PizzaCardProps) {
   return <div></div>;
 }
